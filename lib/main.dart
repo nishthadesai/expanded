7169d95c809214993923e98f67b354479ea7985e
nishthadesai <nishthadesai1611@gmail.com>
2023-04-26 10:38:03 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -5,45 +5,82 @@ runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    home: Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-        Expanded(
-          flex: 8,
-          child: Container(
-          //  constraints: BoxConstraints.expand(),
-            color: Colors.red,
-            child: Center(
-              child: Text('8', //style: TextStyle(color: Colors.white, fontSize: 30,),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: SafeArea(
+              child: Column(
+                children: [
+                  _customcon(
+                    no: 8,col: Colors.red
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Row(
+                      children: [
+                        _customcon(
+                            no: 5,col: Colors.indigo
+                        ),
+                        Expanded(
+                          flex: 3,  //width
+                          child: Column(
+                            children: [
+                              Expanded(
+                                flex: 2,
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: Column(
+                                        children: [
+                                          _customcon(
+                                              no: 1,col: Colors.green
+                                          ),
+                                          _customcon(
+                                              no: 1,col: Colors.lightBlueAccent
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    _customcon(
+                                        no: 2,col: Colors.brown
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              _customcon(
+                                  no: 3,col: Colors.purple
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               )
-          ),
           )
+      ),
+    );
+  }
+}
+Widget _customcon({
+  required int no,
+  required Color col,
+})
+{
+  return Expanded(
+      flex: no,
+      child: Container(
+        color: col,
+        child: Center(
+            child: Text(
+              '${no}', style: TextStyle(color: Colors.white, fontSize: 30,),
+            )
         ),
-          Expanded(
-          flex: 5,
-          child: Container(
-          //constraints: BoxConstraints.expand(),
-          color: Colors.indigo,
-          child: Center(child:
-          Text('5',
-          //style: TextStyle(
-          //color: Colors.white,
-          //fontSize: 30,
-          ),
-          )
-          ),
-          )
-          //)
-          ],
-          )
       )
-    ),
-          );
-          }
-          }
+  );
+}
